373d4011113c85e1c79ed81c9d875e34db325283
MrAlM <81728811+ALMAZIK17364@users.noreply.github.com>
2024-07-17 18:02:15 +0800

    Update module1hard.py
    Правки приняты к сведению

--- module1hard.py
+++ module1hard.py
@@ -1,10 +1,11 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]] #Оценки
+for i in range(len(grades)):
+    grades[i] = sum(grades[i])/len(grades[i])
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'} # Имена
 students = sorted(list(students))
 averageScore = {}
 
 for i in range(5):
-
     averageScore[students[i]] = grades[i]
 
 print(averageScore)
